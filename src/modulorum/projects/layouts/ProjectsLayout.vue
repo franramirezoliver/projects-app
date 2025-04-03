f865d9185168ca6fb6041e856fdec537d6756616
Francisco Ramírez <ramirezoliverf@gmail.com>
2025-04-03 17:39:33 +0200

    Creando el menú de la interface

---
+++ src/modulorum/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <SupraMenu />
+  <main>
+    <LateralisMenu />
+    <router-view />
+  </main>
+</template>
+
+<script lang="ts" setup>
+import SupraMenu from '@/modulorum/commune/components/SupraMenu.vue';
+import LateralisMenu from '../components/LateralisMenu.vue';
+</script>
