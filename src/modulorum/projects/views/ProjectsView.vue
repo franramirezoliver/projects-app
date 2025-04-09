48933c6ed4222c436f5f3ab757fe8af4aec3dd16
Francisco Ramírez <ramirezoliverf@gmail.com>
2025-04-09 18:34:53 +0200

    Ventana modal con Input

--- src/modulorum/projects/views/ProjectsView.vue
+++ src/modulorum/projects/views/ProjectsView.vue
@@ -21,6 +21,9 @@
       </tbody>
     </table>
   </div>
+
+  <InputModal />
+
   <fab-button @click="() => console.log('clicked')">
     <AddCircle />
   </fab-button>
@@ -28,5 +31,6 @@
 
 <script lang="ts" setup>
 import FabButton from '@/modulorum/commune/components/FabButton.vue';
+import InputModal from '@/modulorum/commune/components/InputModal.vue';
 import AddCircle from '@/modulorum/commune/icons/AddCircle.vue';
 </script>
